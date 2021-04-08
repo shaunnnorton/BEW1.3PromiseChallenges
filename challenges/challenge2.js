f40bf76061055639d1c16baba210fb59d67f2823
Shaun Norton <shaunnorton87@gmail.com>
2021-04-07 22:39:14 -0700

    Challenge 2

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -6,15 +6,24 @@
  * 1. Read over the code that follows. What will be printed to the console when
  *    it runs? Run the code using `node challenge2.js` and verify that your
  *    expectation was correct.
- * 
+ *     
+ *    What will be printed to the console is:
+ *      "Hello there, Ducky"
+ *      "MAKE SCHOOL IS AWSOME!!!"
+ *     CORRECT
  * 
  * 2. What happens if greet() fails? Make it fail by changing 'name' to a number
  *    instead of a string. What happens? Does uppercaser() still run?
- * 
+ *    
+ *    If greet fails it will return the error "Name must be a string" which will
+ *    be printed when the promise is called. Uppercaser() will not run.
  * 
  * 3. What happens if greet() succeeds and uppercaser() fails? Modify your code
  *    to achieve this result by changing the values of 'name' and 'my_str' and
  *    run the code again.
+ * 
+ *    If greet succedes and upppercaser fails "Recieved an error!" and "Arguement
+ *    to upper caser must be a string" will print after the result from greet()
  * 
  * 
  * 4. Write a method that takes a string as input and returns the input string
@@ -63,8 +72,32 @@ function uppercaser(str) {
     });
 }
 
-name = 'Ducky'
-my_str = 'Make School is Awesome!!!'
+
+const spacer = (str) => {
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+    if (typeof str === 'string') {
+      let new_string = ''
+      for(let i=0 ; i< str.length * 2; i++){ //Spacing String
+        if(i % 2 === 0){
+          new_string += ' ' 
+        }else{
+          new_string += str[Math.floor(i/2)]
+          //console.log(str[Math.floor(i/2)])
+          //console.log(new_string)
+        }
+      }
+      //console.log(new_string)
+      resolve(new_string)
+    }else{
+      reject("Arguement must be string")
+    }
+    }, 2000)
+  })
+}
+
+name = "Ducky"
+my_str = "Makeschool is Awsome!!!"
 
 greet(name)
     .then((greetResult) => {
@@ -73,7 +106,12 @@ greet(name)
     })
     .then((uppercaserResult) => {
         console.log(uppercaserResult)
-    }).catch((err) => {
+        return spacer(uppercaserResult)
+    })
+    .then((spacerResult)=>{
+        console.log(spacerResult)
+    })
+    .catch((err) => {
         console.log('Received an error!')
         console.log(err);
     });
